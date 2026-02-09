77f0af44fa5e680133a663d553aebbc38b42ab5f
Seyfullah Ismailov <seyfullahismailly@gmail.com>
2026-02-09 15:59:14 +0300

    done homework

---
+++ tests/test_failled.py
@@ -0,0 +1,28 @@
+def test_failled1():
+    assert False
+
+def test_failled2():
+    assert False
+
+def test_failled3():
+    assert False
+
+def test_failled4():
+    assert False
+
+def test_failled5():
+    assert False
+
+def test_failled6():
+    assert False
+
+def test_failled7():
+    assert False
+
+def test_failled8():
+    assert False
+
+def test_failled9():
+    assert False
+
+
---
+++ tests/test_pass.py
@@ -0,0 +1,29 @@
+def test_pass1():
+    assert True
+
+def test_pass2():
+    assert True
+
+def test_pass3():
+    assert True
+
+def test_pass4():
+    assert True
+
+def test_pass5():
+    assert True
+
+def test_pass6():
+    assert True
+
+def test_pass7():
+    assert True
+
+def test_pass8():
+    assert True
+
+def test_pass9():
+    assert True
+
+def test_pass10():
+    assert True
